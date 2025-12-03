8db907c59b784499ba7509a56c76758d09f0511d
Rokkipoliisi <164552630+Rokkipoliisi@users.noreply.github.com>
2025-12-03 12:46:09 +0500

    Delete page.tsx

--- page.tsx
+++
@@ -1,9 +0,0 @@
-import { BouncingRingSimulation } from "@/components/bouncing-ring-simulation";
-
-export default function Home() {
-  return (
-    <div className="min-h-screen">
-      <BouncingRingSimulation />
-    </div>
-  );
-}
